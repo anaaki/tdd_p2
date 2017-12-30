4df846e258bbb94f9c0067c4292f077928370232
= <=>
2017-12-30 23:40:03 +0900

    setUpでwasRunをsetupし、コンストラクタをシンプルにする

--- xunit.py
+++ xunit.py
@@ -13,14 +13,11 @@ class TestCase:
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
-
     def testMethod(self):
         self.wasRun = 1
 
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
 
