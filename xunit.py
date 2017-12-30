7b7562ab117b1b40d5341546050781c2d68cdc28
= <=>
2017-12-31 01:29:32 +0900

    was  runはlogを見ることで判断するので削除

--- xunit.py
+++ xunit.py
@@ -15,11 +15,9 @@ class TestCase:
 
 class WasRun(TestCase):
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod " 
 
     def setUp(self):
-        self.wasRun = None
         self.log = "setUp "
 
 
